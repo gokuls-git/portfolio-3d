aa2b4b7c459ac2cdad8a383602e0d6b0cf633977
gokuls-git <gokul.kumarrs7@gmail.com>
2025-04-07 22:00:41 +0530

    rename

--- src/constants/index.js
+++ src/constants/index.js
@@ -284,32 +284,12 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
 export const workExperiences = [
   {
     id: 1,
-    name: "Framer",
-    pos: "Lead Web Developer",
-    duration: "2022 - Present",
+    name: "Webandcrafts",
+    pos: "UI Developer",
+    duration: "2022 - 2025",
     title:
-      "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
-    icon: "/assets/framer.svg",
+      "At Webandcrafts, I developed high-performance websites and web applications using React.js, Next.js, and GSAP. I crafted immersive 3D experiences with Three.js, optimized performance and SEO, and collaborated with cross-functional teams to deliver seamless, responsive user interfaces.",
+    icon: "https://webandcrafts.com/image/logo1.svg",
     animation: "victory",
-  },
-  {
-    id: 2,
-    name: "Figma",
-    pos: "Web Developer",
-    duration: "2020 - 2022",
-    title:
-      "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
-    icon: "/assets/figma.svg",
-    animation: "clapping",
-  },
-  {
-    id: 3,
-    name: "Notion",
-    pos: "Junior Web Developer",
-    duration: "2019 - 2020",
-    title:
-      "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
-    icon: "/assets/notion.svg",
-    animation: "salute",
   },
 ];
